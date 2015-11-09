5cae82abb79cebb176ccc825689786ab1c561062
= <=>
2015-11-09 10:52:43 -0500

    Perfecting TestServer.py

--- TestServer.py
+++ TestServer.py
@@ -1,5 +1,5 @@
-import flask #import library containing everything we need
-app = flask.Flask(__name__) #idk... just do this to make an application I think?
+from flask import Flask #import library containing everything we need, now WORKS!
+app = Flask(__name__) #idk... just do this to make an application I think?
 
 
 @app.route("/") #if you got to UrL_NAME_SOMETHINg.SOMETHING/ the method below fires
@@ -8,4 +8,4 @@ def mainpage():
 
 if __name__ == "__main__": #if you run this program, ACTIVATE! (so we can import this safely)
     app.run(port=80) #port 80 means the default page. if it's not 80, say 5000 then we have to go to 127.0.0.1:5000 to make it work
-    print "hello world" #cuz fuck it, why not?
+    print ("hello world") #cuz fuck it, why not?
