bcf6141373beedb065d7b6c5238170fae5f88897
kynes446 <mattlee446@gmail.com>
2015-11-10 00:56:01 -0500

    basic template

--- TestServer.py
+++ TestServer.py
@@ -1,10 +1,13 @@
 from flask import Flask #import library containing everything we need, now WORKS!
+from flask import render_template
+
+
 app = Flask(__name__) #idk... just do this to make an application I think?
 
 
 @app.route("/") #if you got to UrL_NAME_SOMETHINg.SOMETHING/ the method below fires
 def mainpage(): 
-    return "Wassup Noobs, u finna get PK'd by Joe and Sid?"
+    return render_template('index.html')
 
 if __name__ == "__main__": #if you run this program, ACTIVATE! (so we can import this safely)
     app.run(port=80) #port 80 means the default page. if it's not 80, say 5000 then we have to go to 127.0.0.1:5000 to make it work
