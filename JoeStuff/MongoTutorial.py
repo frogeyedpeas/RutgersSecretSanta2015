c7bd5100cd995fd62e091001e7f04d0bcdcc9d5d
= <=>
2015-11-10 00:46:30 -0500

    Making test script executable

--- JoeStuff/MongoTutorial.py
+++ JoeStuff/MongoTutorial.py
@@ -1,11 +1,15 @@
 from pymongo import MongoClient
 
-client = MongoClient("localhost:27019") # creates a client
-db = client.primer                      # assigning primer database to db
-db = client['primer']                   # or use this! MAGIC!
+def MongoTest():
+    client = MongoClient("localhost:27019") # creates a client
+    db = client.primer                      # assigning primer database to db
+    db = client['primer']                   # or use this! MAGIC!
+    
+    db.dataset                              # access the dataset collection
+    db['dataset']                           # or like this! MAGIC!
+    
+    coll = db.dataset                       # assign the dataset collection to coll
+    coll = db.['dataset']                   # or type this! MAGIC!
 
-db.dataset                              # access the dataset collection
-db['dataset']                           # or like this! MAGIC!
-
-coll = db.dataset                       # assign the dataset collection to coll
-coll = db.['dataset']                   # or type this! MAGIC!
+if __NAME__ is "__MAIN__":
+    app.MongoTest()
