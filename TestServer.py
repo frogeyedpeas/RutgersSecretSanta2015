4bc9ff82d94eb9dbb3ded30d9947262ac04f4565
= <joebuchoff@gmail.com>
2015-11-22 20:30:18 -0500

    Created html template, updated TestServer.py

--- TestServer.py
+++ TestServer.py
@@ -2,9 +2,16 @@ from flask import Flask #import library containing everything we need, now WORKS
 app = Flask(__name__) #idk... just do this to make an application I think?
 
 
-@app.route("/") #if you got to UrL_NAME_SOMETHINg.SOMETHING/ the method below fires
+@app.route('/') #if you got to UrL_NAME_SOMETHINg.SOMETHING/ the method below fires
+@app.route('/index')
 def mainpage(): 
-    return "Wassup Noobs, u finna get PK'd by Joe and Sid?"
+    print ("COOL BEANZ MAAAN!")
+    return render_template('index.html')
+	
+@app.route('/getData', methods=['POST'])
+def getData():
+	print("I have the data!")
+	print(request.form['firstname']+'\n'+request.form['lastname'])
 
 if __name__ == "__main__": #if you run this program, ACTIVATE! (so we can import this safely)
     app.run(port=80) #port 80 means the default page. if it's not 80, say 5000 then we have to go to 127.0.0.1:5000 to make it work
