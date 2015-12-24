2b0e791baeb1c39f3f6a8f2a8fcea169ba540a8f
Jayant Ghoshal <jayantghoshal@Jayants-MacBook-Pro.local>
2015-12-24 01:20:29 -0500

    fetching and storing data

--- TestServer.py
+++ TestServer.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template #import library containing everything we need, now WORKS!
-import mongo
+from flask import request
 
 app = Flask(__name__) #idk... just do this to make an application I think?
 
+
+#f = open('afile.txt','w') #for writing to be used once only
 
 @app.route('/') #if you got to UrL_NAME_SOMETHINg.SOMETHING/ the method below fires
 @app.route('/index')
@@ -14,6 +16,10 @@ def mainpage():
 def getData():
 	print("I have the data!")
 	print(request.form['firstname']+'\n'+request.form['lastname'])
+	f = open('afile.txt','w')
+	f.write(str(request.form)+"\n")
+	f.close()
+	return render_template('index.html')
 
 if __name__ == "__main__": #if you run this program, ACTIVATE! (so we can import this safely)
     app.run(debug=True, port=80) #port 80 means the default page. if it's not 80, say 5000 then we have to go to 127.0.0.1:5000 to make it work
